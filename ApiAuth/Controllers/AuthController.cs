3dcf969e1a48f70a0283de5bfd6c690acaf9b3e2
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-09-24 19:36:14 +0300

    Add project files.

---
+++ ApiAuth/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using ApiAuth.Buisness;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet("login",Name ="Login")]
+        public IActionResult Login(string userName, string password)
+        {
+            TokenHandler._configuration = _configuration;
+            return Ok(userName == "test" && password == "123456" ? TokenHandler.CreateAccessToken() : new UnauthorizedResult());
+            return Ok();
+        }
+
+        //[HttpGet(Name = "GetWeatherForecastAuth")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
---
+++ ApiProduct/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var _configuration = builder.Configuration;
+SymmetricSecurityKey signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Security"]));
+
+//string authenticationProviderKey = "Bearer";
+//builder.Services.AddAuthentication(option => option.DefaultAuthenticateScheme = authenticationProviderKey)
+//    .AddJwtBearer(authenticationProviderKey, options =>
+//    {
+//        options.RequireHttpsMetadata = false;
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = signInKey,
+//            ValidateIssuer = true,
+//            ValidIssuer = _configuration["JWT:Issuer"],
+//            ValidateAudience = true,
+//            ValidAudience = _configuration["JWT:Audience"],
+//            ValidateLifetime = true,
+//            ClockSkew = TimeSpan.Zero,
+//            RequireExpirationTime = true
+//        };
+//    });
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = signInKey,
+            ValidateIssuer = true,
+            ValidIssuer = _configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = _configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            RequireExpirationTime = true
+        };
+    });
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
